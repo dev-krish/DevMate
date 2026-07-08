c003330de10312bc7796ab0c359fbd8c2d3b67f7
Krish <krishnendu.dutta.dev@gmail.com>
2026-07-08 18:26:01 +0530

    Add Docker support with Dockerfile, dockerignore, and docker-compose configuration

--- main.py
+++ main.py
@@ -6,32 +6,40 @@ from telegram.ext import (
 )
 
 from config import BOT_TOKEN
+from logger import logger
 
 from handlers.start import start
 from handlers.help import help_command
 from handlers.echo import echo
-from handlers.help import help_command
 from handlers.about import about
 from handlers.ping import ping
-from handlers.tools import uuid_command
-from handlers.tools import password_command
-from handlers.tools import hash_command
+from handlers.tools import (
+    uuid_command,
+    password_command,
+    hash_command,
+)
 
-app = ApplicationBuilder().token(BOT_TOKEN).build()
 
-# Commands
-app.add_handler(CommandHandler("start", start))
-app.add_handler(CommandHandler("hello", help_command))
-app.add_handler(CommandHandler("help", help_command))
-app.add_handler(CommandHandler("about", about))
-app.add_handler(CommandHandler("ping", ping))
-app.add_handler(CommandHandler("uuid", uuid_command))
-app.add_handler(CommandHandler("password", password_command))
-app.add_handler(CommandHandler("hash", hash_command))
+def main():
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
 
-# Messages
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("hello", help_command))
+    app.add_handler(CommandHandler("help", help_command))
+    app.add_handler(CommandHandler("about", about))
+    app.add_handler(CommandHandler("ping", ping))
+    app.add_handler(CommandHandler("uuid", uuid_command))
+    app.add_handler(CommandHandler("password", password_command))
+    app.add_handler(CommandHandler("hash", hash_command))
 
-print("🚀 DevMate is running...")
+    app.add_handler(
+        MessageHandler(filters.TEXT & ~filters.COMMAND, echo)
+    )
 
-app.run_polling()
+    logger.info("🚀 DevMate started!")
+
+    app.run_polling()
+
+
+if __name__ == "__main__":
+    main()
